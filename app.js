7a5659c75e26f9b99c8b29be735b48349175e930
testuser <test@test.com>
2019-12-03 19:54:10 -0500

    ok to REST APIs chapter

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 /* var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users'); */
 
 var app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017/express_app', function() {
+  console.log('Connection has been made');
+})
+.catch(err => {
+  console.error('App starting error:', err.stack);
+  process.exit(1);
+});
 
 var fs = require('file-system');
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 module.exports.controller = (app) => {
   // get homepage
   app.get('/', (req, res) => {
-    res.render('index', { title: 'Express' });
+    res.render('users', { title: 'Users' });
   })
 }
